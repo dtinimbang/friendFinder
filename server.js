f138fbd2825a91ddd8e89d6ce1c8af0211e7882d
don <don.tinimbang@gmail.com>
2018-03-08 12:23:05 -0600

    added server.js

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 var express = require('express');
 var bodyParser=require('body-parser');
 var path = require('path');
+
+var app = express();
+
+
+//=====code below used from hot restaurant
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+require("app/routing/apiRoutes")(app);
+require("app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
