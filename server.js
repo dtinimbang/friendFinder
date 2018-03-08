ba5c1b42618d6cc716ffaa0be6777906848ee42b
don <don.tinimbang@gmail.com>
2018-03-08 15:25:06 -0600

    error in server.js, cannot start node, cannot find api routes

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-require("app/routing/apiRoutes")(app);
-require("app/routing/htmlRoutes")(app);
+require('./app/routing/apiroutes.js')(app); 
+require('./app/routing/htmlroutes.js')(app);
 
 // =============================================================================
 // LISTENER
